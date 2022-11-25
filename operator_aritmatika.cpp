9f0a979a3af190d2c0402682c51614938698bb5c
Ramatamoan <ramadanff991@gmail.com>
2022-11-25 13:34:17 +0700

    prubahan 2

---
+++ operator_aritmatika.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std ;
+
+int main(){
+    int a , b, c ;
+    
+    cout<<"Masukan nilai A :";
+    cin>>a;
+    cout<<"Masukan nilai b :";
+    cin>>b;
+    
+    c = a + b ;
+    
+    cout<<"niali a + b = " << c <<endl;
+    
+    return 0 ;
+}
